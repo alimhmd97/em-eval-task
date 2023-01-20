d960aead9a90fb785528e3f6d3e73a13c62c49e3
Ali Mohamed <amohamed@onesolutionc.com>
2023-01-20 10:55:31 +0200

    init project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PostsComponent } from './posts/posts.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path: '', component: PostsComponent}];
+// { path: 'first-component', component: FirstComponent },
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
